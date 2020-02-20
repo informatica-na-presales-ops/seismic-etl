733f83e21e866a77ff7386f1c7fde50bab8ddad3
William Jackson <william@subtlecoolness.com>
2020-02-20 10:31:45 -0600

    Use text instead of uuid
    I'm using an ETL tool that doesn't work with UUID columns in Postgres

--- seismic-etl.sql
+++ seismic-etl.sql
@@ -1,41 +1,41 @@
 create schema seismic;
 
 create table seismic.user_activity (
-    id uuid primary key,
+    id text primary key,
     action text,
     action_type text,
     occurred_at timestamp,
     modified_at timestamp,
-    user_id uuid,
+    user_id text,
     user_username text,
     instance_name text,
     instance_format text,
     is_bound_delivery boolean,
     total_pages integer,
-    content_id uuid,
-    library_content_id uuid,
-    library_content_version_id uuid,
-    workspace_content_id uuid,
-    workspace_content_version_id uuid,
-    livesend_link_id uuid,
-    livesend_link_content_id uuid,
-    generated_live_doc_id uuid,
-    content_profile_id uuid,
-    context_id uuid,
+    content_id text,
+    library_content_id text,
+    library_content_version_id text,
+    workspace_content_id text,
+    workspace_content_version_id text,
+    livesend_link_id text,
+    livesend_link_content_id text,
+    generated_live_doc_id text,
+    content_profile_id text,
+    context_id text,
     context_name text,
     context_type text,
     context_system_type text
 );
 
 create table seismic.search_history (
-    id uuid primary key,
-    search_cycle_id uuid,
+    id text primary key,
+    search_cycle_id text,
     search_term_raw text,
     search_term_normalized text,
     search_type text,
     occurred_at timestamp,
     modified_at timestamp,
-    user_id uuid,
+    user_id text,
     result_count integer,
     result_count_doc_center integer,
     result_count_news_center integer,
@@ -46,7 +46,7 @@ create table seismic.search_history (
 );
 
 create table seismic.livesend_links (
-    id uuid primary key,
+    id text primary key,
     is_separate_send boolean,
     notification_mode text,
     allow_download boolean,
@@ -55,32 +55,33 @@ create table seismic.livesend_links (
     has_password boolean,
     created_at timestamp,
     modified_at timestamp,
-    created_by uuid,
+    created_by text,
     url_path text
 );
 
 create table seismic.livesend_link_contents (
-    id uuid primary key,
+    id text primary key,
     created_at timestamp,
-    created_by uuid,
-    livesend_link_id uuid,
+    created_by text,
+    livesend_link_id text,
     name text,
     modified_at timestamp,
     total_pages integer
 );
 
 create table seismic.livesend_link_members (
-    livesend_link_id uuid,
-    external_user_id uuid,
+    livesend_link_id text,
+    external_user_id text,
     modified_at timestamp,
     created_at timestamp,
-    email text
+    email text,
+    primary key (livesend_link_id, external_user_id)
 );
 
 create table seismic.livesend_page_views (
-    id uuid primary key,
-    livesend_link_content_id uuid,
-    livesend_viewing_session_id uuid,
+    id text primary key,
+    livesend_link_content_id text,
+    livesend_viewing_session_id text,
     occurred_at timestamp,
     modified_at timestamp,
     session_started_at timestamp,
@@ -90,12 +91,12 @@ create table seismic.livesend_page_views (
 );
 
 create table seismic.contents (
-    id uuid primary key,
-    context_id uuid,
+    id text primary key,
+    context_id text,
     context_name text,
     context_system_type text,
     created_at timestamp,
-    created_by uuid,
+    created_by text,
     doc_center_url text,
     format text,
     is_cart_content boolean,
@@ -103,30 +104,30 @@ create table seismic.contents (
     is_deleted boolean,
     is_published boolean,
     latest_library_content_version_created_at timestamp,
-    latest_library_content_version_id uuid,
+    latest_library_content_version_id text,
     latest_library_content_version_size integer,
     latest_workspace_content_version_created_at timestamp,
-    latest_workspace_content_version_id uuid,
-    library_content_id uuid,
+    latest_workspace_content_version_id text,
+    library_content_id text,
     library_url text,
     materialized_path text,
     modified_at timestamp,
     name text,
     news_center_url text,
-    origin_content_profile_id uuid,
-    owner_id uuid,
-    preview_image_id uuid,
+    origin_content_profile_id text,
+    owner_id text,
+    preview_image_id text,
     published_version_expires_at timestamp,
     teamsite_id text,
-    thumbnail_image_id uuid,
+    thumbnail_image_id text,
     type text,
     version text
 );
 
 create table seismic.users (
-    id uuid primary key,
+    id text primary key,
     created_at timestamp,
-    default_content_profile_id uuid,
+    default_content_profile_id text,
     default_content_profile_name text,
     deleted_at timestamp,
     email text,
@@ -146,77 +147,77 @@ create table seismic.users (
 );
 
 create table seismic.user_property_assignments (
-    user_id uuid,
-    user_property_id uuid,
+    user_id text,
+    user_property_id text,
     user_property_name text,
     user_property_value text
 );
 
 create table seismic.group_members (
-    group_id uuid,
+    group_id text,
     group_name text,
-    user_id uuid
+    user_id text
 );
 
 create table seismic.content_property_assignments (
-    content_property_id uuid,
+    content_property_id text,
     content_property_name text,
     content_property_type text,
     content_property_value text,
-    library_content_id uuid
+    library_content_id text
 );
 
 create table seismic.generated_live_docs (
-    id uuid primary key,
+    id text primary key,
     created_at timestamp,
-    library_content_id uuid,
-    library_content_version_id uuid,
+    library_content_id text,
+    library_content_version_id text,
     modified_at timestamp,
-    preview_image_id uuid,
-    thumbnail_image_id uuid,
-    user_id uuid
+    preview_image_id text,
+    thumbnail_image_id text,
+    user_id text
 );
 
 create table seismic.generated_live_doc_components (
-    id uuid primary key,
+    id text primary key,
     created_at timestamp,
     format text,
-    generated_live_doc_id uuid,
+    generated_live_doc_id text,
     insert_method text,
     insert_status text,
-    library_content_id uuid,
+    library_content_id text,
     library_content_name text,
-    library_content_version_id uuid,
+    library_content_version_id text,
     modified_at timestamp,
-    parent_component_id uuid,
-    thumbnail_image_id uuid
+    parent_component_id text,
+    thumbnail_image_id text
 );
 
 create table seismic.generated_live_doc_slides (
-    id uuid primary key,
-    generated_live_doc_id uuid,
+    id text primary key,
+    generated_live_doc_id text,
     modified_at timestamp,
     page_short_id text,
-    parent_component_id uuid,
+    parent_component_id text,
     position integer,
     title text
 );
 
 create table seismic.generated_live_doc_fields (
-    id uuid primary key,
+    id text primary key,
     field_name text,
     field_type text,
     field_value text,
-    generated_live_doc_id uuid,
+    generated_live_doc_id text,
     modified_at timestamp
 );
 
 create table seismic.generated_live_doc_output_formats (
-    id uuid primary key,
+    id text primary key,
     compatibility text,
     file_name text,
     format text,
-    generated_live_doc_id uuid,
+    generated_live_doc_id text,
     has_clear_notes boolean,
     has_open_password boolean,
     has_owner_options boolean,
